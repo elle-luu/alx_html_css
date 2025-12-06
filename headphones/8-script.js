65f0ca5e5a463e82777d7dd1ba7087330ce5161f
Elle-Shavor Senoya Luu <senoyaluu1@gmail.com>
2025-12-06 20:13:50 +0000

    add Humberger Menu

---
+++ headphones/8-script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");  
+    navMenu.classList.toggle("open");      
+});
